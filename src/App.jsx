8e60411edcb0f040e89c0a673d6707b0cc1c98e2
Tuyife <omotuyifeisrael@gmail.com>
2026-04-13 05:28:01 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import About from './components/About';
 import Footer from './components/Footer';
-
+import privacy from './components/Privacy';
+import PrivacyPolicy from './components/Privacy';
 function App() {
   return (
     <div className="App">
       <About />
+      <PrivacyPolicy/>
       <Footer/>
     
     </div>
