6993957a02a2e3d0f9e96b22fcce4b4e7bc70214
Tuyife <omotuyifeisrael@gmail.com>
2026-04-13 05:11:37 -0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import About from './components/About';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <About />
+      <Footer/>
+    
+    </div>
+  );
+}
+
+
+export default App;
